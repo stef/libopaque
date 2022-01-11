a2197e06567809b172810c0dbd5c42be43685f37
stf <7o5rfu92t@ctrlc.hu>
2022-01-11 22:31:43 +0000

    [mod] removed obsolete old cruft

--- go/.arch/opaque_bindings.c
+++
@@ -1,9 +0,0 @@
-#include "opaque_bindings.h"
-#include <stdio.h>
-
-int greet(const char *name, int year, char *out) {
-    int n;
-    n = sprintf(out, "Greetings, %s from %d! We come in peace :)", name, year);
-
-    return n;
-}
--- go/.arch/opaque_bindings.h
+++
@@ -1,6 +0,0 @@
-#ifndef _OPAQUE_BINDING_H
-#define _OPAQUE_BINDING_H
-
-int greet(const char *name, int year, char *out);
-
-#endif

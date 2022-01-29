f53d2bf421c90a49ca04534615f8bfa0bc9c8435
stf <7o5rfu92t@ctrlc.hu>
2022-01-29 22:26:19 +0000

    [enh] adapted ruby bindings

--- ruby/test.rb
+++ ruby/test.rb
@@ -4,27 +4,25 @@ require './opaque.so'
 
 include Opaque
 
-cfg = [InSecEnv, InSecEnv, InSecEnv, InSecEnv, InSecEnv]
 idU = "idU"
 idS = "idS"
 pwd = "pwd"
+context = "context"
 
 # create a record "directly on the server"
-rec, export_key = register(pwd, idU, idS, cfg)
+rec, export_key = register(pwd, idU, idS)
 
 # client initiates session
 secU, pub = create_credential_request(pwd)
 
 # server sets up session and responds with record
-resp, skS, secS = create_credential_response(pub, rec, idU, idS, cfg)
+resp, skS, secS = create_credential_response(pub, rec, idU, idS, context)
 
 # client recovers keys, sets up session
-skU, authU, export_keyU, idUc, idSc = recover_credentials(resp, secU, cfg)
+skU, authU, export_keyU = recover_credentials(resp, secU, context, pub, idU, idS)
 
 raise "fail" unless skS == skU
 raise "fail" unless export_key == export_keyU
-raise "fail" unless idU == idUc
-raise "fail" unless idS == idSc
 
 # server authenticates client explicitly
 raise "fail" unless user_auth(secS, authU)
@@ -34,49 +32,47 @@ m, secU = create_registration_request(pwd)
 
 secS, pub = create_registration_response(m)
 
-rec, export_key = finalize_request(secU, pub, idU, idS, cfg)
+rec, export_key = finalize_request(secU, pub, idU, idS)
 
 rec = store_user_record(secS, rec)
 
 secU, pub = create_credential_request(pwd)
 
 # server sets up session and responds with record
-resp, skS, secS = create_credential_response(pub, rec, idU, idS, cfg)
+resp, skS, secS = create_credential_response(pub, rec, idU, idS, context)
 
 # client recovers keys, sets up session
-skU, authU, export_keyU, idUc, idSc = recover_credentials(resp, secU, cfg)
+skU, authU, export_keyU = recover_credentials(resp, secU, context, pub, idU, idS)
 
 raise "fail" unless skS == skU
 raise "fail" unless export_key == export_keyU
-raise "fail" unless idU == idUc
-raise "fail" unless idS == idSc
 
 # server authenticates client explicitly
 raise "fail" unless user_auth(secS, authU)
 
 # start 4 step registration with 1k server setup
-pkS, skS = create_server_keys()
+_, skS = create_server_keys()
 
 m, secU = create_registration_request(pwd)
 
-secS, pub = create_1k_registration_response(m, pkS)
+secS, pub = create_registration_response(m, skS)
 
-rec, export_key = finalize_request(secU, pub, idU, idS, cfg)
+rec, export_key = finalize_request(secU, pub, idU, idS)
 
-rec = store_1k_user_record(secS, skS, rec)
+rec = store_user_record(secS, rec)
 
 secU, pub = create_credential_request(pwd)
 
 # server sets up session and responds with record
-resp, skS, secS = create_credential_response(pub, rec, idU, idS, cfg)
+resp, skS, secS = create_credential_response(pub, rec, idU, idS, context)
 
 # client recovers keys, sets up session
-skU, authU, export_keyU, idUc, idSc = recover_credentials(resp, secU, cfg)
+skU, authU, export_keyU = recover_credentials(resp, secU, context, pub, idU, idS)
 
 raise "fail" unless skS == skU
 raise "fail" unless export_key == export_keyU
-raise "fail" unless idU == idUc
-raise "fail" unless idS == idSc
 
 # server authenticates client explicitly
 raise "fail" unless user_auth(secS, authU)
+
+print "all ok\n"

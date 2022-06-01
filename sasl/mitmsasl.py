8293cfa49cc5e99beba4a0ed70088ab9673b3d36
stf <7o5rfu92t@ctrlc.hu>
2022-06-01 17:44:48 +0000

    [mod] removed unneeded import from mitmsasl

--- sasl/mitmsasl.py
+++ sasl/mitmsasl.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, If not, see <http://www.gnu.org/licenses/>.
 
-from mitmproxy import ctx
 from mitmproxy.http import Headers
 import requests, sys, binascii, sasl, subprocess
 
